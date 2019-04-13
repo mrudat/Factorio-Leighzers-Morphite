73252b5b1e3265fdcdb1245f0eeadb547802bfaf
Martin Rudat <mrudat@toraboka.com>
2019-04-13 15:39:49 +1000

    Add support for deadlock_stacking

--- Source/data-final-fixes.lua
+++ Source/data-final-fixes.lua
@@ -3,3 +3,25 @@ if settings.startup["ismorphiteToExternalModEnabled"].value then --if enabled in
 end
 
 InsertData()--this is where all recipes+technology get loaded into data.raw
+
+local itemTable =
+{
+  {"morphite-ore", 1},
+  {"morphite-fuel", 2}
+}
+
+if deadlock_stacking then
+  for _, item in pairs(itemTable) do
+    if data.raw.item[item[1]] then
+      deadlock_stacking.create(item[1], "__leighzermorphite__/graphics/"..item[1].."-stack.png", "deadlock-stacking-"..item[2], 32)
+    end
+  end
+end
+
+if deadlock_crating then
+  for _, item in pairs(itemTable) do
+    if data.raw.item[item[1]] then
+      deadlock_crating.create(item[1], "deadlock-crating-"..item[2])
+    end
+  end
+end
